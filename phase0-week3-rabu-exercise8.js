23f7aa6104100f105abfc7eb7ef5967ca6eb13db
noviirna <novi.irnawati@gmail.com>
2019-03-18 16:52:34 +0700

    tukar besar kecil

---
+++ phase0-week3-rabu-exercise8.js
@@ -0,0 +1,29 @@
+function tukarBesarKecil(kalimat) {
+    // you can only write your code here!
+    arrKalimat = kalimat.split("");
+    var panjangString = arrKalimat.length;
+    var result = [];
+    var isLowCase = kalimat.toLowerCase();
+    var isUpCase = kalimat.toUpperCase();
+    
+    for(var i = 0; i < panjangString; i++){
+        if(arrKalimat[i] == isLowCase[i]){
+            result.push(isUpCase[i]);
+        }
+        else if(arrKalimat[i] == isUpCase[i]){
+            result.push(isLowCase[i]);
+        }
+        else{
+            result.push(arrKalimat[i]);
+        }
+    }
+    result = result.join("");
+    return result;
+}
+  
+  // TEST CASES
+  console.log(tukarBesarKecil('Hello World')); // "hELLO wORLD"
+  console.log(tukarBesarKecil('I aM aLAY')); // "i Am Alay"
+  console.log(tukarBesarKecil('My Name is Bond!!')); // "mY nAME IS bOND!!"
+  console.log(tukarBesarKecil('IT sHOULD bE me')); // "it Should Be ME"
+  console.log(tukarBesarKecil('001-A-3-5TrdYW')); // "001-a-3-5tRDyw"
