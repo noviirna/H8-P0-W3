affd0fd4bd4b13d3acf6441843328e375bc79ac4
noviirna <novi.irnawati@gmail.com>
2019-03-19 11:13:11 +0700

    exercise 10 change Me - add comments into code

--- phase0-week3-rabu-exercise10.js
+++ phase0-week3-rabu-exercise10.js
@@ -1,64 +1,75 @@
 function changeMe(arr) {
-  // you can only write your code here!
-
-
-
-  //put it inside
-  for(var i =0; i < arr.length; i++){
-    var fullName = '';
-    var fN = '';
-    var lN = '';
-    var g = '';
-    var a = '';
-    for(var j = 0; j< arr[i].length; j++){
-      switch(j){
-        case 0 :
-          fullName = fullName + arr[i][j] + ' ';
-          fN = arr[i][j];
+    // you can only write your code here!
+    //move the array[][] into variable then put it inside of the object
+    for(var i =0; i < arr.length; i++){
+      var fullName = ''; //full name
+      var fN = ''; //first name
+      var lN = ''; //last name
+      var g = ''; //gender
+      var a = ''; //age
+      //looping to assign each data from the array into expected variable 
+      for(var j = 0; j< arr[i].length; j++){
+        //because the array's pattern is [firstname, last name, gender, yearofborn], use conditional switch case
+        switch(j){
+          case 0 :
+            fullName = fullName + arr[i][j] + ' '; //assign first name data  into fullName variable
+            fN = arr[i][j]; // assign first name data into fN variable
+            break;
+          case 1 :
+            fullName = fullName + arr[i][j]; //assign last name data into fullName variable
+            lN = arr[i][j]; // assign last name data into lN variable
+            break;
+          case 2 :
+            g = arr[i][j]; // assign gender data into g variable
+            break;
+          case 3 :
+            a = 2019 - arr[i][j]; // assign age result into a variable
+            break;
+          default :
           break;
-        case 1 :
-          fullName = fullName + arr[i][j];
-          lN = arr[i][j];
-          break;
-        case 2 :
-          g = arr[i][j];
-          break;
-        case 3 :
-          a = arr[i][j];
-          break;
-        default :
-        break;
-      }//end of conditional switch-case
+        }//end of conditional switch-case
+        
+        //if a is empty but not null, then assign string 'Invalid birth year' into a variable
+        if(a == ''){
+          a = 'Invalid birth year'
+        }//end of conditional if
+  
+      }//end of loop array[][]
 
-      if(a == ''){
-        a = 'Invalid birth year'
-      }//end of conditional if
+      //object
+      var myObj =
+      {
+        firstName : fN, //assign fN variable into myObj 'firstName'
+        lastName : lN, //assign ln variable into myObj 'lastName'
+        gender : g, //assign g variable into myObj 'g'
+        age : a, //assign a variable into myObj 'a'
+      }
 
-    }//end of loop array[][]
-    var myObj =
-    {
-      firstName : fN,
-      lastName : lN,
-      gender : g,
-      age : a,
-    }
-    //cetak Full Name & angka
-    console.log(i+1 +'. ' + fullName + ':');
-    console.log(myObj);
-    console.log('\n');
-  } //end of loop array[]
+      //print fullname + counter
+      console.log(i+1 +'. ' + fullName + ':');
 
-}// end of function
+      //print the object
+      console.log(myObj);
+      console.log('\n');
 
-// TEST CASES
-changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]); // 1. Christ Evans:
-// { firstName: 'Christ',
-//   lastName: 'Evans',
-//   gender: 'Male',
-//   age: 37 }
-// 2. Robert Downey:
-// { firstName: 'Robert',
-//   lastName: 'Downey',
-//   gender: 'Male',
-//   age: 'Invalid Birth Year' }
-changeMe([]); // ""
+    } //end of loop array[]
+  
+  }// end of function
+  
+  // TEST CASES
+  changeMe(
+      [
+        ['Christ', 'Evans', 'Male', 1982],
+        ['Robert', 'Downey', 'Male']
+      ]
+    ); // 1. Christ Evans:
+  // { firstName: 'Christ',
+  //   lastName: 'Evans',
+  //   gender: 'Male',
+  //   age: 37 }
+  // 2. Robert Downey:
+  // { firstName: 'Robert',
+  //   lastName: 'Downey',
+  //   gender: 'Male',
+  //   age: 'Invalid Birth Year' }
+  changeMe([]); // ""
