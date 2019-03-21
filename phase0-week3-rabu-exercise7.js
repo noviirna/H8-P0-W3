775b7a26270e87b54cf5a8ddd844be52c777c4ae
noviirna <novi.irnawati@gmail.com>
2019-03-21 13:00:09 +0700

    urutkan abjad, ada dua cara yang bisa diterapkan

--- phase0-week3-rabu-exercise7.js
+++ phase0-week3-rabu-exercise7.js
@@ -1,24 +1,41 @@
+// cara ke-satu, fungsi urutkan abjad dengan 
 function urutkanAbjad(str) {
-    var arr = str.split('');
-    // you can only write your code here!
-    for(var batas = arr.length-1; batas >= 0; batas--){  
-        for(var now = 0; now <= batas; now++){
-          var next = now + 1;
-          var temp = '';
-          if(arr[now] > arr[next]){ 
-            temp = arr[next];
-            arr[next] = arr[now];
-            arr[now] = temp;
-          }
+  var myAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  var result = ''
+  for (var i = 0; i < myAlphabet.length; i++){
+    for(var j = 0; j < str.length; j++){
+      if(myAlphabet[i] == str[j]){
+        result = result + myAlphabet[i]
+      }
+    }
+  }
+  return result;
+}
+      
+// TEST CASES
+console.log(urutkanAbjad('hello')); // 'ehllo'
+console.log(urutkanAbjad('truncate')); // 'acenrttu'
+console.log(urutkanAbjad('developer')); // 'deeeloprv'
+console.log(urutkanAbjad('software')); // 'aeforstw'
+console.log(urutkanAbjad('aegIs')); // 'Iaegs'
+
+/*
+// cara ke-dua, fungsi urutkan abjad denan membuat algoritma sorting dan bantuan variable array
+function urutkanAbjad(str) {
+  var arr = str.split('');
+  // you can only write your code here!
+  for(var batas = arr.length-1; batas >= 0; batas--){  
+      for(var now = 0; now <= batas; now++){
+        var next = now + 1;
+        var temp = '';
+        if(arr[now] > arr[next]){ 
+          temp = arr[next];
+          arr[next] = arr[now];
+          arr[now] = temp;
         }
       }
-    str = arr.join('');
-    return str;
     }
-      
-      // TEST CASES
-      console.log(urutkanAbjad('hello')); // 'ehllo'
-      console.log(urutkanAbjad('truncate')); // 'acenrttu'
-      console.log(urutkanAbjad('developer')); // 'deeeloprv'
-      console.log(urutkanAbjad('software')); // 'aeforstw'
-      console.log(urutkanAbjad('aegis')); // 'aegis'
+  str = arr.join('');
+  return str;
+  }
+*/
