f4310b078369db538dd7cd285c6a9da2052e2ff4
noviirna <novi.irnawati@gmail.com>
2019-03-26 09:57:35 +0700

    makan terus rekursif, dua approach

--- phase0-week3-jumat-exercise15.js
+++ phase0-week3-jumat-exercise15.js
@@ -1,8 +1,9 @@
+//approach 1
 function makanTerusRekursif(waktu) {
   // you can only write your code here!
   var count = 0;
   if(waktu == 0 ){
-    return 0;
+    return count;
   }
   else if(waktu < 15){
     count++
@@ -23,3 +24,25 @@ console.log(makanTerusRekursif(10)); // 1
 console.log(makanTerusRekursif(0)); // 0
 console.log(makanTerusRekursif(35)); // 3
 console.log(makanTerusRekursif(15)); // 1
+
+
+//approach 2
+function makanTerusRekursif(waktu) {
+  // you can only write your code here!
+  if(waktu == 0){
+    return 0
+  }
+  if(waktu <= 15){
+    return 1
+  }
+  if(waktu > 15){
+    return 1 + makanTerusRekursif(waktu-15)
+  }
+}
+
+// TEST CASES
+console.log(makanTerusRekursif(66)); // 5
+console.log(makanTerusRekursif(100)); // 7
+console.log(makanTerusRekursif(90)); // 6
+console.log(makanTerusRekursif(10)); // 1
+console.log(makanTerusRekursif(0)); // 0
